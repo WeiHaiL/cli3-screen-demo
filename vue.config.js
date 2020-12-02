f8060823a686602b68f2beb0a89a54c109fbdefa
WeiHaiL <35366825+WeiHaiL@users.noreply.github.com>
2020-12-02 16:27:12 +0800

    Update vue.config.js
    更改配置

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,35 @@ const resolve = (dir) => {
   return path.join(__dirname, dir)
 }
 module.exports = {
+  lintOnSave: true,
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'less',
       patterns: [path.resolve(__dirname, './src/assets/css/global.less')],
     }
-  }
+  },
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [path.resolve(__dirname, './src/assets/css/global.less')],
+    }
+  },
+  productionSourceMap: false,
+  devServer: {
+    contentBase: path.resolve(__dirname, './Cesium'),
+    open: true,
+    proxy: {
+      '/szxc_jshc': {
+        // target: 'http://10.80.70.40:8092/szxc_jshc/', // 肖奎
+        // target: "http://szny.agri.hangzhou.gov.cn:8078/szxc_ladp/", // 正式
+        target: "http://61.153.188.179:8082/nypt_jshc/ ", // 测试
+        ws: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/szxc_jshc': '/', // 重写接口
+        },
+      },
+    },
+  },
 }
