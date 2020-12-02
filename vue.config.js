43538cfca1e123bbb75101f02b7377837ad95d11
WeiHaiL <35366825+WeiHaiL@users.noreply.github.com>
2020-12-02 16:28:26 +0800

    Update vue.config.js
    更改配置

--- vue.config.js
+++ vue.config.js
@@ -19,17 +19,14 @@ module.exports = {
   },
   productionSourceMap: false,
   devServer: {
-    contentBase: path.resolve(__dirname, './Cesium'),
     open: true,
     proxy: {
-      '/szxc_jshc': {
-        // target: 'http://10.80.70.40:8092/szxc_jshc/', // 肖奎
-        // target: "http://szny.agri.hangzhou.gov.cn:8078/szxc_ladp/", // 正式
+      '/nypt_jshc': {
         target: "http://61.153.188.179:8082/nypt_jshc/ ", // 测试
         ws: false,
         changeOrigin: true,
         pathRewrite: {
-          '^/szxc_jshc': '/', // 重写接口
+          '^/nypt_jshc': '/', // 重写接口
         },
       },
     },
